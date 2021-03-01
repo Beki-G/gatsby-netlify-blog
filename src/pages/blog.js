52db94b358ccfaa82704421ed7c2b345d31d295c
Izuku <63758052+Beki-G@users.noreply.github.com>
2021-02-28 16:46:20 -0800

    Add /blog and markdown support

---
+++ src/pages/blog.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import BlogRoll from '../components/BlogRoll'
+
+const BlogIndexPage = () =>{
+    return (
+        <React.Fragment>
+          <h1>Latest Posts</h1>
+          <section>
+            <div className="content">
+              <BlogRoll />
+            </div>
+          </section>
+        </React.Fragment>
+      )
+}
+
+export default BlogIndexPage;
