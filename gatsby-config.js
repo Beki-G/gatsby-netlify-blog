895f66cd565c53cfcb2bc31cffce1bfc843f8c31
Izuku <63758052+Beki-G@users.noreply.github.com>
2021-03-02 08:25:21 -0800

    add cloudinary to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,58 +1,45 @@
-// const activeEnv = process.env.GATSBY_ACTIVE_ENV || process.env.NODE_ENV || "development"
-
-// require("dotenv").config({
-//   path: `.env.${activeEnv}`,
-// })
-require("dotenv").config()
-
 module.exports = {
   plugins: [
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/assets`,
-        name: 'uploads',
+        name: "uploads",
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     path: `${__dirname}/static/assets`,
-    //     name: 'assets',
-    //   },
-    // },
+
     {
-      resolve:`gatsby-source-filesystem`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name:`images`,
-        path: `${__dirname}/static/assets`
-      }
+        name: `images`,
+        path: `${__dirname}/static/assets`,
+      },
     },
+
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
       },
     },
-    
-    
+
     `gatsby-transformer-sharp`,
+
     `gatsby-plugin-sharp`,
+
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          
           {
-            resolve:`gatsby-remark-images`, 
+            resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 200,
             },
-            
-        },
-        `gatsby-plugin-netlify-cms-paths`,
+          },
+          `gatsby-plugin-netlify-cms-paths`,
         ],
       },
     },
@@ -63,7 +50,13 @@ module.exports = {
         name: `markdown-pages`,
       },
     },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/cms.js`,
+      },
+    },
     `gatsby-plugin-netlify-cms`,
-    `gatsby-plugin-postcss`
+    `gatsby-plugin-postcss`,
   ],
 }
