cb3b8c9f7ff6bf1ff988ca9c41dcddf8681b0fa3
Izuku <63758052+Beki-G@users.noreply.github.com>
2021-03-02 18:11:39 -0800

    Add content to index.html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,19 @@
 import React from "react"
+import CardGrid from "../components/CardGrid.js"
+import Footer from "../components/Footer.js"
+import Hero from "../components/Hero/Hero.js"
+import Profile from "../components/Profile.js"
 // import { Link } from "gatsby"
-import Navbar from "../components/Navbar"
+// import Navbar from "../components/Navbar"
 
 export default () => {
   return (
     <div>
-      <Navbar />
 
+      <Hero />
+      <Profile />
+      <CardGrid />
+      <Footer />
       <p>
         {/* <Link to="/account">My Account</Link> */}
       </p>
---
+++ src/pages/projects.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Projects = () => {
+    return (
+        <div>
+            <Navbar />
+            <p className="font-extrabold text-3xl text-center">Projects</p>
+        </div>
+    )
+}
+
+export default Projects
